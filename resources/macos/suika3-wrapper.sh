9925ef3cbeb1fea4ec514a75cc470f5459bfb7a2
Awe Morris <awe@noctvm.io>
2026-04-24 07:42:58 +0900

    Add macOS VS Code integration

---
+++ resources/macos/suika3-wrapper.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ "$1" == "suika3" ]; then
+    BASE="$(readlink -f .)";
+    FILE="$BASE/misc/macos/suika3";
+    xattr -c "$FILE";
+    chmod +x "$FILE";
+    if "$FILE"; then
+        exit 1;
+    fi;
+    exit 0;
+fi
+
+if [ "$1" == "suika3-pack" ]; then
+    BASE="$(readlink -f .)";
+    FILE="$BASE/misc/macos/suika3-pack";
+    xattr -c "$FILE";
+    chmod +x "$FILE";
+    if "$FILE" $(ls -A | grep -vE '^(misc|suika3.exe|.vscode)$'); then
+        exit 1;
+    fi;
+    exit 0;
+fi
