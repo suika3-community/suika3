deb118467ea3ce7a1f18902d4790609ac76e9c5d
Awe Morris <awe@noctvm.io>
2026-04-24 04:07:39 +0900

    Support Android auto-build on VS Code on macOS

--- resources/projects/android/build.sh
+++ resources/projects/android/build.sh
@@ -12,10 +12,8 @@ SRC_DIR="../../"
 rm -rf "$TARGET_ASSETS_DIR"
 mkdir -p "$TARGET_ASSETS_DIR"
 find "$SRC_DIR" \
-    -name ".vscode" -prune -o \
-    -name "misc" -prune -o \
-    -name "suika3.exe" -false -o \
-    -name "assets.arc" -false -o \
+    \( -name ".vscode" -o -name "misc" \) -prune -o \
+    \( -name "suika3.exe" -o -name "assets.arc" \) -o \
     -type f -print | while read -r file; do
         rel_path="${file#$SRC_DIR}"
         dest_file="$TARGET_ASSETS_DIR/$rel_path"
@@ -29,6 +27,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Downloading JDK..."
+export CUR_DIR=$(pwd)
 if [ "$OS" == "Linux" ]; then
     if [ "$ARCH" == "arm64" ]; then
         if [ ! -d jdk-17.0.1.jdk ]; then
@@ -70,7 +69,7 @@ fi
 
 echo "Downloading Android SDK..."
 if [ "$OS" == "Linux" ]; then
-    if [ ! -d commandlinetools-linux-11076708_latest ]; then
+    if [ ! -d cmdline-tools ]; then
         if [ ! -f commandlinetools-linux-11076708_latest.zip ]; then
             curl -L -O https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip;
         fi
@@ -78,21 +77,22 @@ if [ "$OS" == "Linux" ]; then
     fi
 fi
 if [ "$OS" == "Darwin" ]; then
-    if [ ! -d commandlinetools-mac-11076708_latest ]; then
+    if [ ! -d cmdline-tools ]; then
         if [ ! -f commandlinetools-mac-11076708_latest.zip ]; then
             curl -L -O https://dl.google.com/android/repository/commandlinetools-mac-11076708_latest.zip;
         fi
+        unzip commandlinetools-mac-11076708_latest.zip;
     fi
-    unzip commandlinetools-mac-11076708_latest.zip;
 fi
 
 echo "Setting up Android SDK..."
-export SDK_TEMP_ROOT=$CURRENT_DIR/android-sdk
-yes | ./cmdline-tools/bin/sdkmanager --install "cmdline-tools;latest" --sdk_root=$SDK_TMP_ROOT
-set ANDROID_SDK_ROOT=$SDK_TEMP_ROOT
-set ANDROID_HOME=$SDK_TEMP_ROOT
-set NEW_SDKMANAGER=$ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager
-yes | $NEW_SDKMANAGER --install --licenses --sdk_root=$ANDROID_SDK_ROOT
+export SDK_TEMP_ROOT=$CUR_DIR/android-sdk
+yes | ./cmdline-tools/bin/sdkmanager --install "cmdline-tools;latest" --sdk_root=$SDK_TEMP_ROOT
+export ANDROID_SDK_ROOT=$SDK_TEMP_ROOT
+export ANDROID_HOME=$SDK_TEMP_ROOT
+export NEW_SDKMANAGER=$ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager
+yes | $NEW_SDKMANAGER --install --sdk_root=$ANDROID_SDK_ROOT
+yes | $NEW_SDKMANAGER --licenses --sdk_root=$ANDROID_SDK_ROOT
 yes | $NEW_SDKMANAGER "platforms;android-35" "platform-tools" "build-tools;35.0.0" --sdk_root=$ANDROID_SDK_ROOT
 echo "sdk.dir=$ANDROID_SDK_ROOT" > local.properties
 
@@ -105,7 +105,7 @@ echo "  Debug:   $CUR_DIR/app/build/outputs/apk/debug/app-debug.apk"
 echo "  Release: $CUR_DIR/app/build/outputs/apk/release/app-release-unsigned.apk"
 
 echo
-export PACKAGE_NAME=`$ANDROID_HOME/build-tools/35.0.0/aapt2.exe dump packagename $CUR_DIR/app/build/outputs/apk/debug/app-debug.apk`
+export PACKAGE_NAME=`$ANDROID_HOME/build-tools/35.0.0/aapt2 dump packagename $CUR_DIR/app/build/outputs/apk/debug/app-debug.apk`
 echo "Package Name: $PACKAGE_NAME"
 echo
 echo "If you want to change the package name, edit applicationId in $CUR_DIR/app/build.gradle"
@@ -116,13 +116,13 @@ read s
 if [ "$s" == "y" ]; then
     echo
     echo Uninstalling existing APK...
-    $ANDROID_HOME/platform-tools/adb.exe uninstall $PACKAGE_NAME
+    $ANDROID_HOME/platform-tools/adb uninstall $PACKAGE_NAME
 
     echo Installing new APK...
-    $ANDROID_SDK_ROOT/platform-tools/adb.exe install -r $CUR_DIR/app/build/outputs/apk/debug/app-debug.apk
+    $ANDROID_SDK_ROOT/platform-tools/adb install -r $CUR_DIR/app/build/outputs/apk/debug/app-debug.apk
 
     echo Starting app...
-    $ANDROID_HOME/platform-tools/adb.exe shell am start -n $PACKAGE_NAME/io.noctvm.playfield.engineandroid.MainActivity
+    $ANDROID_HOME/platform-tools/adb shell am start -n $PACKAGE_NAME/io.noctvm.playfield.engineandroid.MainActivity
 fi
 
 echo
