2ae89ff596593342263fbd7909c8660160edf9b0
Awe Morris <awe@noctvm.io>
2026-04-30 10:26:26 +0900

    Fix wrapper scripts for VSCode

--- resources/linux/suika3-wrapper.sh
+++ resources/linux/suika3-wrapper.sh
@@ -1,29 +1,43 @@
 #!/bin/sh
 
-if [ "$1" == "suika3" ]; then
+if [ -z "$1" ]; then
+    echo "Specify an option.";
+fi
+
+if [ "$1" = "suika3" ]; then
     if [ -n "`which suika3`" ]; then
         exec suika3;
     fi;
     if flatpak info "vn.suika3.engine" > /dev/null 2>&1; then
-        exec flatpak run vn.suika3.engine;
+        exec flatpak run vn.suika3.engine .;
     else
-        flatpak install SDK/linux/Suika3-x86_64.flatpak;
-        if flatpak info "vn.suika3.engine" > /dev/null 2>&1; then
-            exec flatpak run vn.suika3.engine;
+        echo -n "Do you want to install Suika3 via Flathub? <y/N>";
+        read s;
+        if [ "$s" = "y" ]; then
+            flatpak install --user -y SDK/linux/Suika3-x86_64.flatpak;
+            flatpak override --user --filesystem=host vn.suika3.engine;
+        else
+            exit 1;
         fi;
+        exec flatpak run vn.suika3.engine .;
     fi;
 fi
 
-if [ "$1" == "suika3-pack" ]; then
+if [ "$1" = "suika3-pack" ]; then
     if [ -n "`which suika3-pack`" ]; then
         exec suika3-pack $(ls -A | grep -vE '^(SDK|suika3.exe|.vscode|assets.arc)$');
     fi;
     if flatpak info "vn.suika3.engine" > /dev/null 2>&1; then
         exec flatpak run --command=suika3-pack vn.suika3.engine $(ls -A | grep -vE '^(SDK|suika3.exe|.vscode|assets.arc)$');
     else
-        flatpak install SDK/linux/Suika3-x86_64.flatpak;
-        if flatpak info "vn.suika3.engine" > /dev/null 2>&1; then
-            exec flatpak run --command=suika3-pack vn.suika3.engine $(ls -A | grep -vE '^(SDK|suika3.exe|.vscode|assets.arc)$');
+        echo -n "Do you want to install Suika3 via Flathub? <y/N>";
+        read s;
+        if [ "$s" = "y" ]; then
+            flatpak install --user -y SDK/linux/Suika3-x86_64.flatpak;
+            flatpak override --user --filesystem=host vn.suika3.engine;
+        else
+            exit 1;
         fi;
+        exec flatpak run --command=suika3-pack vn.suika3.engine $(ls -A | grep -vE '^(SDK|suika3.exe|.vscode|assets.arc)$');
     fi;
 fi
--- resources/macos/suika3-wrapper.sh
+++ resources/macos/suika3-wrapper.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ "$1" == "suika3" ]; then
+if [ "$1" = "suika3" ]; then
     BASE="$(readlink -f .)";
-    FILE="$BASE/misc/macos/suika3";
+    FILE="$BASE/SDK/macos/suika3";
     xattr -c "$FILE";
     chmod +x "$FILE";
     if "$FILE"; then
@@ -11,12 +11,12 @@ if [ "$1" == "suika3" ]; then
     exit 0;
 fi
 
-if [ "$1" == "suika3-pack" ]; then
+if [ "$1" = "suika3-pack" ]; then
     BASE="$(readlink -f .)";
-    FILE="$BASE/misc/macos/suika3-pack";
+    FILE="$BASE/SDK/macos/suika3-pack";
     xattr -c "$FILE";
     chmod +x "$FILE";
-    if "$FILE" $(ls -A | grep -vE '^(misc|suika3.exe|.vscode)$'); then
+    if "$FILE" $(ls -A | grep -vE '^(SDK|assets.arc|suika3.exe|.vscode)$'); then
         exit 1;
     fi;
     exit 0;
